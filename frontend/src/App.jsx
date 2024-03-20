425493d4ec98868183a884e788a1e3d60607272a
Vipin <vipin333kvk@gmail.com>
2024-03-20 22:40:46 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Heading from './components/Heading'
+
+function App() {
+  
+
+  return (
+    <>
+     <Heading></Heading> 
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Heading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Heading = () => {
+  return (
+    <div>
+      {/* Heading file h */}
+      <h1 className='text-6xl text-center mt-3 font-bold  '> Doctor Diabetes </h1>
+    </div>
+  )
+}
+
+export default Heading
